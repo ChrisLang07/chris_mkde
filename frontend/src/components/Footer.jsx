5c8f72c57b4b85ac9e53fdb2fb89bc77bf86b9de
ChrisLang07 <christophe.langlois.one@gmail.com>
2025-01-14 18:06:55 +0100

    Footer.jsx - Year changed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer>
             <span className='footer-text'>
-            © 2024 Christophe LANGLOIS. Tous droits réservés.
+            © 2025 Christophe LANGLOIS. Tous droits réservés.
                 <span>&#169;</span>
             </span>
         </footer>
