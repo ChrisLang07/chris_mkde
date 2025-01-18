e9e0ccb7eae1288d10500b37b335d492701a8d8e
ChrisLang07 <christophe.langlois.one@gmail.com>
2025-01-18 20:33:26 +0100

    404 Page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './pages/Layout';
-import Home from'./pages/Home';
+import Home from './pages/Home';
+import NoPage from './pages/NoPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />} >
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
+        {/* Route pour les pages non trouvées */}
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
