a5f0da3a2a3caa36e43a12ed56a94b6c9f1fa1d1
ChrisLang07 <christophe.langlois.one@gmail.com>
2025-03-15 18:10:45 +0100

    SCSS for PolicyContent,CookieConsent added + Schema.org

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import NoPage from './pages/NoPage';
+import PolicyContent from './pages/PolicyContent';
+import CookieConsent from './components/CookieConsent'; 
 
 function App() {
   return (
@@ -9,13 +11,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/politique-de-confidentialite" element={<PolicyContent />} />
         </Route>
-        {/* Route pour les pages non trouvées */}
         <Route path="*" element={<NoPage />} />
       </Routes>
+      <CookieConsent /> 
     </BrowserRouter>
   );
 }
 
 export default App;
-
